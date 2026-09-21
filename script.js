3718e95f8fb26465ae8140ee93886779de1f4de7
Jose Luis Carrillo Igea <carrillo.162006@e.unavarra.es>
2026-09-21 18:02:43 +0200

    Cambio la pagina por completo para que sea pelis

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Este es el script");
+
+document.getElementById('boton-buscar').addEventListener('click', function() {
+    const titulo = document.getElementById('buscador').value;
+    const apiKey = '6748eca6';
+    const url = `https://www.omdbapi.com/?apikey=${apiKey}&t=${titulo}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            const resultado = document.getElementById('resultado');
+            
+            if (datos.Response === "True") {
+                resultado.innerHTML = `<strong>Director:</strong> ${datos.Director} <br> <strong>Año:</strong> ${datos.Year}`;
+            } else {
+                resultado.innerHTML = "Película no encontrada.";
+            }
+        })
+        .catch(error => {
+            console.error("Hubo un error en la petición:", error);
+        });
+});
